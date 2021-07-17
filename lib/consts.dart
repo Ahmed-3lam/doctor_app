913cfc84b71ce96700d2dfe19158d48d42f768fa
Ahmed Allam <85360095+Ahmed-3lam@users.noreply.github.com>
2021-07-17 02:40:25 +0200

    Update consts.dart

--- lib/consts.dart
+++ lib/consts.dart
@@ -4,4 +4,4 @@ import 'package:flutter/material.dart';
 
 String doctorImage = "https://scontent.faly3-2.fna.fbcdn.net/v/t1.6435-9/153132418_5107729769269739_3570070721466563618_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=GO-gtWHrKIkAX9lVRgo&_nc_ht=scontent.faly3-2.fna&oh=df12a7a987a379eb040b48959f9c8b50&oe=60F58041";
 String doctorDescription= "جراحة اطفال ، ابو الريش،. القصر العيني ، زميل كلية الجراحين الملكية بانجلترا ، عضو الجمعية المصرية لجراحة الاطفال";
-String locationApi ="AIzaSyCdBLg_f2VDVd1CLxBs68q4kC6pD1UO6KM";
+String locationApi =API;
